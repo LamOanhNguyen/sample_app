a85928f8f21197df38bb0d009bda5797b96955d0
Lam Oanh Nguyen <nguyenthilamoanh37@gmail.com>
2018-04-14 00:09:20 +0700

    chapter7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  def show
+    @user = User.find params[:id]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      # Handle a successful save.
+    else
+      render "new"
+    end
+  end
 end
