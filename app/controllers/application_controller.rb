10f50c2105eca63b9bed46db8c39f7027a0502bf
Lam Oanh Nguyen <nguyenthilamoanh37@gmail.com>
2018-04-14 12:29:14 +0700

    chapter8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,4 @@
 class ApplicationController < ActionController::Base
-  def show
-    @user = User.find params[:id]
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new params[:user]
-    if @user.save
-      # Handle a successful save.
-    else
-      render "new"
-    end
-  end
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
